e26be8cf1f683b4de87a6e106f3f54f03f8e01f7
박정현 <pjh9996@naver.com>
2023-07-11 20:56:20 +0900

    Updates

---
+++ _next/static/GXn-zTVS3NhpNQb2gbtLk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fc23598b59459b3d.css","static/chunks/pages/index-99fb0a1b05819150.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
